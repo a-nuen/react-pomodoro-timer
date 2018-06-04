850d9a548fbd9f80f8f7826584cfe5e7008f51bf
Alexander Nguyen <alexandernguyen@192-168-1-8.tpgi.com.au>
2018-06-05 01:17:22 +1000

    duno

--- src/components/break.js
+++ src/components/break.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const Break = props => {
-    return (
-        <div className='timerBreak'>
-            <h2>Break</h2>
-            <div className='flex'>
-                <button className='plusMinus' onClick={props.breakMinusMinute} >-</button>
-                <p>{props.break /60}</p>
-                <button className='plusMinus' onClick={props.breakAddMinute} >+</button>
-            </div>
-        </div>
-    )
-    
+	return (
+		<div className='timerBreak'>
+			<h2>Break</h2>
+			<div className='flex'>
+				<button className='plusMinus' onClick={props.breakMinusMinute} >-</button>
+				<p>{props.break /60}</p>
+				<button className='plusMinus' onClick={props.breakAddMinute} >+</button>
+			</div>
+		</div>
+	)
 }
 
 export default Break;
