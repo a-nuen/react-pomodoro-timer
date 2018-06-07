9066556a02ef5997195ac95803f37f6f2c11795a
Alexander Nguyen <alexandernguyen@192-168-1-7.tpgi.com.au>
2018-06-07 16:37:04 +1000

    refactor

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-const Timer = props => {
-    return(
-        <div className='timerBreak'>
-            <h2>Time</h2>
-            <div className='flex'>
-                <button className='plusMinus' onClick={props.timerMinusMinute} >-</button>
-                    <p>{props.timer /60}</p>
-                <button className='plusMinus' onClick={props.timerAddMinute} >+</button>
-            </div>
-        </div>
-    )
-}
+const Timer = props => (
+	<div className='timerBreak'>
+		<h2>Time</h2>
+		<div className='flex'>
+			<button className='plusMinus' onClick={props.timerMinusMinute} >-</button>
+				<p>{props.timer /60}</p>
+			<button className='plusMinus' onClick={props.timerAddMinute} >+</button>
+		</div>
+	</div>
+)
+
 
 export default Timer;
